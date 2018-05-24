5a45e326ec2d7f06ab994b82ff769051b9efa29e
github username <kenny.hom27@gmail.com>
2018-05-23 23:52:47 -0400

    finished section 1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import SearchBar from './components/SearchBar'
+
+
+const API_KEY = "AIzaSyCX_J5mm35cszd4wRJlG3-Kik2TssjqY0o"
+
+const App = () => {
+  return (
+    <div>
+      <SearchBar/>
+    </div>
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
