5a0257ba2eff0a88afd858998175b5ab514625ae
github username <kenny.hom27@gmail.com>
2018-05-24 12:41:38 -0400

    fixed style and added videos

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search'
 import SearchBar from './components/SearchBar'
+import VideoList from './components/VideoList'
 
 
 const API_KEY = "AIzaSyCX_J5mm35cszd4wRJlG3-Kik2TssjqY0o"
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar/>
-    </div>
-  )
+
+class App extends Component {
+  state = {
+    videos: []
+  }
+
+  componentDidMount() {
+    YTSearch({key: API_KEY, term: 'dogs'}, (videos) => {
+      this.setState({videos})
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar/>
+        <VideoList videos={this.state.videos}/>
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
